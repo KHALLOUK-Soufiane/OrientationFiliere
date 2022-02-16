8217db43f3d49bc60ca5fa128c8a6ca9328f60f5
alimakouar <makouarali@gmail.com>
2022-02-16 22:12:26 +0100

    change avatar img

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -14,7 +14,7 @@
                 <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <div class="media align-items-center">
                         <span class="avatar avatar-sm rounded-circle">
-                        <img alt="Image placeholder" src="{{ asset('argon') }}/img/theme/team-1-800x800.jpg">
+                        <img alt="Image placeholder" src="{{ asset('assets/img/theme/user.jpg') }}">
                         </span>
                     </div>
                 </a>
